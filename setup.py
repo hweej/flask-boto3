4019286aae9d0162f34d26a3a567e7e8b51731d0
Joel Steres <joel@ymobility.com>
2018-04-20 14:47:01 -0700

    Update setup for pip 10

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from pip.req import parse_requirements
+try: # for pip >= 10 (courtesy of https://stackoverflow.com/a/49867265)
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup
 
 # Requirements
