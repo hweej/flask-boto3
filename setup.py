417bb5e85f6fcacea9fd545ec3d9f2b8c2630771
Joel Steres <joel@ymobility.com>
2021-06-03 00:54:00 -0700

    Update dependencies including flask 2.0 peer upgrade.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='Flask-Boto3',
-    version='0.3.2m',
+    version='0.3.2n',
     url='https://github.com/Ketouem/flask-boto3',
     license='MIT',
     author='Cyril "Ketouem" Thomas',
