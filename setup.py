682262dd14e305c81e730283e587b86b4e1c83f5
Joel Steres <joel@ymobility.com>
2021-07-29 20:23:56 -0700

    Update boto3 version constraint.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='Flask-Boto3',
-    version='0.3.2n',
+    version='0.3.2o',
     url='https://github.com/Ketouem/flask-boto3',
     license='MIT',
     author='Cyril "Ketouem" Thomas',
